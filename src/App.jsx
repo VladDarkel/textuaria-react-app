e97c1ba39beede1227d9fa66da331b877b516182
Vlad Boikiv <vladboykiv@gmail.com>
2025-02-03 22:56:02 +0200

    Some tags deleted

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,12 @@ function App() {
 
   return (
     <>
-      <p>Hello</p>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
